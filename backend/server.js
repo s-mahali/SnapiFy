164c4df5fee4c5ad4862bd44da9c103aab6c677f
s-mahali <mahalisoumen839@gmail.com>
2025-03-15 14:56:16 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://snapi-fy.vercel.app",
     credentials: true,
 }
 app.use(cors(corsOptions));
